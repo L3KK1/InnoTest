eb3bd533d9f07bcf6ae917ef16700deee0587254
L3KK1 <V.L-2018@yandex.ru>
2024-04-23 20:48:16 +0300

    RecyclerView headers

---
+++ app/src/main/java/com/example/innotest/HomeFragment.kt
@@ -0,0 +1,22 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.innotest.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Загрузка макета для фрагмента
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Дополнительная логика, выполняемая после создания представления фрагмента
+    }
+}
